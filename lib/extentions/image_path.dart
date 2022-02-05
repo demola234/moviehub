96de7b5c127ea349d7e8ac99adcb492b4eedca9f
Kolawole Ademola <64567320+demola234@users.noreply.github.com>
2022-02-05 05:40:41 +0100

    folder structure

---
+++ lib/extentions/image_path.dart
@@ -0,0 +1,13 @@
+class ImagePath {
+  //images Path
+  static const String imageDir = "assets/images";
+  //icons Path
+  static const String iconDir = "assets/icons";
+  //json Path
+  static const String jsonDir = "assets/json";
+
+
+
+  //Images 
+    // static const String LOGO = "$imageDir/filename";
+}
