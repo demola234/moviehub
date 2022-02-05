a2ef5dffef840e6aedc319762ba076bc0043b5db
Kolawole Ademola <64567320+demola234@users.noreply.github.com>
2022-02-05 19:44:06 +0100

    screens updated

--- lib/extentions/routers.dart
+++ lib/extentions/routers.dart
@@ -1,8 +1,12 @@
  import 'package:flutter/material.dart';
+import 'package:moviehub/views/Description/description.dart';
 import 'package:moviehub/views/Home/home_screen.dart';
+import 'package:moviehub/views/Settings/settings.dart';
 
 Map<String, WidgetBuilder> get routes {
     return {
-      'homescreen' : (context) => HomeScreen()
+      'homescreen' : (context) => HomeScreen(),
+      'description' : (context) => Description(),
+      'settings' : (context) => SettingsScreen()
     };
   }
---
+++ lib/views/Description/description.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Description extends StatefulWidget {
+  const Description({ Key? key }) : super(key: key);
+
+  @override
+  _DescriptionState createState() => _DescriptionState();
+}
+
+class _DescriptionState extends State<Description> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
